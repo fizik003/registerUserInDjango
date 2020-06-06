04f6e517957186e8a8e07ca98ceb59b1bb0d6763
fizik003 <kassik0boks@gmail.com>
2020-06-07 02:47:23 +0300

    feat: add app accounts and login/logout

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from account.views import dashboard
+from django.contrib.auth.views import LoginView, LogoutView 
+
+app_name = 'account'
+
+urlpatterns = [
+    path('', dashboard, name='dashboard'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+LOGIN_REDIRECT_URL = 'account:dashboard'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
