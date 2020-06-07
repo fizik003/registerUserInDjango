fe6db271fe2f189b915fadd9e29d4d90a1e1e7cc
fizik003 <kassik0boks@gmail.com>
2020-06-07 18:10:03 +0300

    feat: add reset and change password

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,27 @@
-from django.urls import path
+from django.urls import path, include
 from account.views import dashboard
-from django.contrib.auth.views import LoginView, LogoutView 
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 app_name = 'account'
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uid64>/<token>/', PasswordResetConfirmView.as_view, name='password_reset_confirm'),
+    # path('reset/done', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
