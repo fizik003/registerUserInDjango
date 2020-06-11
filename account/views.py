940f6faf8ca5734ce7ac34412efcbfb7673c0552
fizik003 <kassik0boks@gmail.com>
2020-06-12 02:06:04 +0300

    feat: add messages for user

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
 
@@ -59,9 +60,13 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile update successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
+        
     response = {
         'user_form':user_form,
         'profile_form': profile_form
