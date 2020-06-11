15053a6296005f290b2371a108dcd4b4033aa30a
fizik003 <kassik0boks@gmail.com>
2020-06-12 02:38:40 +0300

    feat:add email authentication backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -102,6 +102,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
